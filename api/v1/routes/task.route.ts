22db20b4554410706d464ad22482482ea6eeb69f
thai <thai@example.com>
2025-11-29 18:51:08 +0700

    Tạo routes

---
+++ api/v1/routes/task.route.ts
@@ -0,0 +1,21 @@
+import {Router, Request, Response} from "express";
+import Task from "../../../models/task.model";
+const router: Router = Router();
+
+
+router.get("/", async(req: Request, res: Response) => {
+    const tasks = await Task.find({
+        deleted: false
+    })
+    res.json(tasks)
+})
+
+router.get("/detail/:id", async(req: Request, res: Response) => {
+    const task = await Task.find({
+        deleted: false,
+        _id: req.params.id
+    })
+    res.json(task)
+})
+
+export const taskRoutes: Router = router;
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
-import Task from "./models/task.model";
+import mainV1Routes from "./api/v1/routes/index.route";
 
 
 dotenv.config();
@@ -11,21 +11,7 @@ database.connect();
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
-app.get("/tasks", async(req: Request, res: Response) => {
-    const tasks = await Task.find({
-        deleted: false
-    })
-    res.json(tasks)
-})
-
-app.get("/tasks/detail/:id", async(req: Request, res: Response) => {
-    const task = await Task.find({
-        deleted: false,
-        _id: req.params.id
-    })
-    res.json(task)
-})
-
+mainV1Routes(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
